5a4ed3979740f41b906f26a0d357b55874c9a293
RichardAri <richardaripari@gmail.com>
2024-05-09 15:34:01 -0500

    implementacion de la red neuronal de prediccion

--- practica/consume-api.py
+++ practica/consume-api.py
@@ -18,3 +18,26 @@ data
 
 #* ------------------------------------------ datos con grafico 
 
+import numpy as np
+import pandas as pd
+import yfinance as yf
+from pandas_datareader import data as pdr
+import matplotlib.pyplot as plt
+
+yf.pdr_override()
+
+tickers = ['BTC-USD','ETH-USD']
+
+start = '2022-12-01'
+end = '2023-12-01'
+
+data = pdr.get_data_yahoo(tickers, start, end)['Close']
+
+data.plot(figsize=(10, 6))
+plt.title('Precios históricos de Bitcoin y Ethereum')
+plt.xlabel('Fecha')
+plt.ylabel('Precio de cierre (USD)')
+plt.grid(True)
+plt.legend(tickers)
+plt.show()
+
