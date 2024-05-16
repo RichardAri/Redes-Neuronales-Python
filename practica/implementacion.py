57224e524e56e985b5560ea3235387c077524cce
RichardAri <richardaripari@gmail.com>
2024-05-16 11:02:35 -0500

    actualizacion de fecha para prediccion

--- practica/implementacion.py
+++ practica/implementacion.py
@@ -1,53 +1,53 @@
-import tensorflow as tf 
-import numpy as np 
-import yfinance as yf 
-from pandas_datareader import data as pdr 
-  
-# Obtener datos 
-yf.pdr_override() 
+import tensorflow as tf
+import numpy as np
+import yfinance as yf
+from pandas_datareader import data as pdr
 
-tickers = ['BTC-USD','ETH-USD'] 
-start = '2023-05-08' 
-end = '2024-05-09' 
+# Obtener datos
+yf.pdr_override()
 
-btc_data = pdr.get_data_yahoo(tickers[0], start, end)['Close'].values.astype(float) 
-eth_data = pdr.get_data_yahoo(tickers[1], start, end)['Close'].values.astype(float) 
+tickers = ['BTC-USD','ETH-USD']
+start = '2023-05-08'
+end = '2024-05-15'
 
-# Escalar datos  
-btc_min = np.min(btc_data) # obtenemos valores minimos 
-btc_max = np.max(btc_data) # y valores maximos 
-eth_min = np.min(eth_data) 
-eth_max = np.max(eth_data) 
-  
-# formula de escalado min/max 
-btc_scaled = (btc_data - btc_min) / (btc_max - btc_min) 
-eth_scaled = (eth_data - eth_min) / (eth_max - eth_min) 
-bitcoin = eth_scaled[:-1] 
-ethereum = btc_scaled[1:] 
+btc_data = pdr.get_data_yahoo(tickers[0], start, end)['Close'].values.astype(float)
+eth_data = pdr.get_data_yahoo(tickers[1], start, end)['Close'].values.astype(float)
 
-# definir modelo 
-modelo = tf.keras.Sequential([ 
-    tf.keras.layers.Dense(units=30, input_shape=[1]), #! 30 y 60 el mejor tiempo hasta ahora 
-    tf.keras.layers.Dense(units=60), 
-    tf.keras.layers.Dense(units=1) 
-]) 
+# Escalar datos 
+btc_min = np.min(btc_data) # obtenemos valores minimos
+btc_max = np.max(btc_data) # y valores maximos
+eth_min = np.min(eth_data)
+eth_max = np.max(eth_data)
 
-# compilar modelo 
-modelo.compile(optimizer=tf.keras.optimizers.Adam(0.01), loss='mean_squared_error') #adam learning rate 
-                                                # 0.0.1 != 0.1 pasos mas cortos 
-                                                # optimizando mas la data 
+# formula de escalado min/max
+btc_scaled = (btc_data - btc_min) / (btc_max - btc_min)
+eth_scaled = (eth_data - eth_min) / (eth_max - eth_min)
 
-# entrenar modelo 
-historial = modelo.fit(bitcoin, ethereum, epochs=1000, verbose=False) # historial guarda la perdida en cada epoca 
+bitcoin = eth_scaled[:-1]
+ethereum = btc_scaled[1:]
 
-# realizar prediccion 
-result = modelo.predict([ethereum[-1]]) 
+# definir modelo
+modelo = tf.keras.Sequential([
+    tf.keras.layers.Dense(units=30, input_shape=[1]), #! 30 y 60 el mejor tiempo hasta ahora
+    tf.keras.layers.Dense(units=60),
+    tf.keras.layers.Dense(units=1)
+])
 
-# desescalamos la prediccion 
-predicted_btc = result * (btc_max - btc_min) + btc_min 
-  
-print("Prediccion: ") 
-print("El proximo precio de Bitcoin es aproximadamente " + str(predicted_btc[0][0]) + " USD!") 
+# compilar modelo
+modelo.compile(optimizer=tf.keras.optimizers.Adam(0.01), loss='mean_squared_error') #adam learning rate
+                                                # 0.0.1 != 0.1 pasos mas cortos
+                                                # optimizando mas la data
+# entrenar modelo
+historial = modelo.fit(bitcoin, ethereum, epochs=1000, verbose=False) # historial guarda la perdida en cada epoca
+
+# realizar predicción
+result = modelo.predict([ethereum[-1]])
+
+# desescalamos la predicción
+predicted_btc = result * (btc_max - btc_min) + btc_min
+
+print("Hagamos una predicción!")
+print("El próximo precio de Bitcoin es aproximadamente " + str(predicted_btc[0][0]) + " USD!")
 
 #print("Variables internas del modelo")
 #print(oculta1.get_weights()) 
